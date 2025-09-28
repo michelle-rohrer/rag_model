8923d4a5d5ea2356af22738d82554eeba8ba2aa0
Michelle <michelle.rohrer18@gmail.com>
2025-09-28 15:22:23 +0200

    Change

--- MC1/src/credentials.py
+++ MC1/src/credentials.py
@@ -1,2 +1,2 @@
-AZURE_OPENAI_API_KEY = 'bd5d63356ecc4812b59c22287288fc1a'
+AZURE_OPENAI_API_KEY = 'bd5d63356ecc4812b59c22287288fc1'
 AZURE_OPENAI_ENDPOINT = 'https://p-oai-htdsnlp-05.openai.azure.com/'
